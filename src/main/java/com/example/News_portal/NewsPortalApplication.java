b8a0b7abca6ce82f90d6bcf92ba410a07eb83b5d
amikolenda <aminakolenda@gmail.com>
2021-01-28 11:37:46 +0100

    Update NewsPortalApplication

--- src/main/java/com/example/News_portal/NewsPortalApplication.java
+++ src/main/java/com/example/News_portal/NewsPortalApplication.java
@@ -1,13 +1,38 @@
 package com.example.News_portal;
 
+import com.example.News_portal.models.Admin;
+import com.example.News_portal.repositories.AdminRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class NewsPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsPortalApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+
+	@Component
+	public class CommandLineAppStartupRunner implements CommandLineRunner {
+		@Autowired
+		AdminRepository adminRepository;
+
+		@Override
+		public void run(String...args) throws Exception {
+			Admin admin = new Admin("admin","admin");
+			adminRepository.save(admin);
+		}
 	}
 
 
