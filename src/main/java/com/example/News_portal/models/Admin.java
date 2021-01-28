7427a621dc197d6fd1315350409230b5f350ca95
amikolenda <aminakolenda@gmail.com>
2021-01-28 11:25:48 +0100

    Create Admin.java

---
+++ src/main/java/com/example/News_portal/models/Admin.java
@@ -0,0 +1,89 @@
+package com.example.News_portal.models;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Table(name = "ADMIN")
+@Entity
+public class Admin implements Serializable {
+    private static final long serialVersionUID = 4241824572545764721L;
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "user_name")
+    @NotNull
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private String role = "ADMIN";
+
+    public Admin() {
+    }
+    public Admin(@NotNull String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Admin setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Admin setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Admin setRole(String role) {
+        this.role = role;
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Admin setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        return Objects.equals(userName, admin.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "userName='" + userName + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
