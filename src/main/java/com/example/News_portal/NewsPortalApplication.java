aad025920bc6b960bc3f5ab6eb4fd762be653a1c
amikolenda <aminakolenda@gmail.com>
2021-01-28 13:41:42 +0100

    Update NewsPortalApplication

--- src/main/java/com/example/News_portal/NewsPortalApplication.java
+++ src/main/java/com/example/News_portal/NewsPortalApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NewsPortalApplication {
@@ -20,6 +22,16 @@ public class NewsPortalApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 
